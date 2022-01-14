aa12fb71a6d0938a2b65b3c461064156ead2f132
Teko-Hashimoto <tekojapa3d@gmail.com>
2022-01-14 11:58:17 -0300

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,5 +1,4 @@
 import MovieScore from "components/MovieScore";
-import { type } from "os";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
@@ -8,8 +7,6 @@ type Props = {
 }
 
 function MovieCard({movie} : Props) {
-
-   
 
     return (
 
